ea09180b04bdfac6b6b38fbc0de4efc8bc457f3c
venlomj <venlo.mj@hotmail.nl>
2024-06-05 20:39:46 +0200

    contact section finished.

--- sections/contact.tsx
+++ sections/contact.tsx
@@ -3,18 +3,42 @@ import { MdEmail, MdSubject } from "react-icons/md";
 import ContactCard from "@/components/cards/contact";
 import Heading from "@/components/heading/heading";
 import Card from "@/components/ui/Card";
-import Input from "@/components/ui/inputs";
+import Input from "@/components/ui/input";
 import Button from "@/components/ui/Button";
 import TextArea from "@/components/ui/text-area";
 import { SiMinutemailer } from "react-icons/si";
 import { budgetOptions, servicesOptions } from "@/data";
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import SelectInput from "@/components/ui/select-input";
+import emailjs from "@emailjs/browser";
 
 export default function ContactSection() {
+  const formRef = useRef<HTMLFormElement>(null!);
+  const btnRef = useRef<HTMLButtonElement>(null);
+
   const [services, setServices] = useState<string[]>([]);
 
   const [budgets, setBudgets] = useState<string[]>([]);
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_pajgfvn",
+        "template_d68f5rg",
+        formRef.current,
+        "PKdWECQ9FnTFVDw9-"
+      )
+      .then(
+        (response) => {
+          console.log(response.text);
+          console.log("Email sent successfully");
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
   return (
     <div className="pt-24 px-3 lg:px-8">
@@ -37,17 +61,32 @@ export default function ContactSection() {
             />
           </div>
           {/* Contact form */}
-          <div className="lg:col-span-2 bg-secondary-background border border-border rounded-lg space-y-3 relative overflow-hidden py-5 px-[25px] shadow-md">
+          <form
+            ref={formRef}
+            onSubmit={sendEmail}
+            className="lg:col-span-2 bg-secondary-background border border-border rounded-lg space-y-3 relative overflow-hidden py-5 px-[25px] shadow-md"
+          >
             <div className="flex flex-col lg:flex-row items-center justify-between mb-4 gap-8">
-              <Input type="text" placeholder="Full Name" icon={<FaUser />} />
+              <Input
+                name="name"
+                type="text"
+                placeholder="Full Name"
+                icon={<FaUser />}
+              />
               <Input
                 type="email"
+                name="email"
                 placeholder="Email Address"
                 icon={<MdEmail />}
               />
             </div>
             <div className="flex items-center justify-between mb-4 gap-8">
-              <Input type="text" placeholder="Subject" icon={<MdSubject />} />
+              <Input
+                name="subject"
+                type="text"
+                placeholder="Subject"
+                icon={<MdSubject />}
+              />
             </div>
             {/* Multiple select wrapper */}
             <div className="flex flex-col gap-6">
@@ -92,16 +131,35 @@ export default function ContactSection() {
             </div>
             {/* TextArea message */}
             <TextArea
+              name="message"
               placeholder="Tell me about your project"
               icon={<FaProjectDiagram />}
             />
             <div className="w-full flex justify-end">
-              <Button className="!w-44 !py-3 !text-xl">
-                Send
-                <SiMinutemailer />
-              </Button>
+              <div onClick={() => btnRef.current?.click()}>
+                <Button className="!w-44 !py-3 !text-xl">
+                  Send
+                  <SiMinutemailer />
+                </Button>
+              </div>
+              {/* Hidden services and budget inputs */}
+              <div className="hidden">
+                <input
+                  type="text"
+                  value={services.join(", ")}
+                  name="services"
+                  hidden
+                />
+                <input
+                  type="text"
+                  value={budgets.join(", ")}
+                  name="budget"
+                  hidden
+                />
+              </div>
+              <button type="submit" hidden ref={btnRef}></button>
             </div>
-          </div>
+          </form>
         </div>
       </Card>
     </div>
