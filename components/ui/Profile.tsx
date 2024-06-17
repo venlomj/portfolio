06d0e3bb5494e6bba50d207f684a6525feb54405
venlomj <venlo.mj@hotmail.nl>
2024-06-17 23:18:33 +0200

    adding old website.

--- components/ui/Profile.tsx
+++ components/ui/Profile.tsx
@@ -4,25 +4,21 @@ import MyPicture from "@/public/assets/images/profile/campnou.jpg";
 
 export default function Profile() {
   return (
-    <div
-      className="flex items-center gap-x-2
-    transition-colors duration-75 text-primary-foreground"
+    <a
+      href="https://venlomj.sinners.be/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center gap-x-2 transition-colors duration-75 text-primary-foreground"
     >
-      {/* Photo */}
-      <div
-        className="relative w-[100px] h-[100px]
-         rounded-full flex items-center justify-center bg-gradient-to-r from-blue-joust
-         to-green-benzol"
-      >
+      <div className="relative w-[100px] h-[100px] rounded-full flex items-center justify-center bg-gradient-to-r from-blue-joust to-green-benzol">
         <Image
           src={MyPicture}
           alt="venlomj"
           className="w-[95px] h-[95px] bottom-[0.2vw] border-blue-cosmos rounded-full object-cover"
         />
-        {/* Online */}
         <div className="w-3 h-3 rounded-full bg-green-benzol border border-blue-cosmos absolute right-0 bottom-5"></div>
       </div>
       <div className="text-3xl font-medium">MURREL VENLO</div>
-    </div>
+    </a>
   );
 }
