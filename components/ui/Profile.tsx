082007fd3869e504bf2b017319c570481c3ec1bb
venlomj <venlo.mj@hotmail.nl>
2024-07-04 09:30:26 +0200

    changing url.

--- components/ui/Profile.tsx
+++ components/ui/Profile.tsx
@@ -5,7 +5,7 @@ import MyPicture from "@/public/assets/images/profile/campnou.jpg";
 export default function Profile() {
   return (
     <a
-      href="https://venlomj.vercel.app/"
+      href="https://venlomj-portfolio.vercel.app/"
       rel="noopener noreferrer"
       className="flex items-center gap-x-2 transition-colors duration-75 text-primary-foreground"
     >
@@ -16,8 +16,9 @@ export default function Profile() {
           className="w-[95px] h-[95px] bottom-[0.2vw] border-blue-cosmos rounded-full object-cover"
           style={{
             maxWidth: "100%",
-            height: "auto"
-          }} />
+            height: "auto",
+          }}
+        />
         <div className="w-3 h-3 rounded-full bg-green-benzol border border-blue-cosmos absolute right-0 bottom-5"></div>
       </div>
       <div className="text-3xl font-medium">MURREL VENLO</div>
